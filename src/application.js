6275848e52bb80034b31ec5e1a27bf4a5426eddb
Julien Muetton <themouette@gmail.com>
2013-06-17 17:45:10 +0000

    Fix #7 : implement event registration for `Application`

--- src/application.js
+++ src/application.js
@@ -19,26 +19,63 @@ define([
         // init factories namespace
         this.factories = {};
         // init event listeners
-        initEventListeners(project, this);
+        initEventListeners(this);
         // finally call initialize method
         this.initialize.call(this, project);
     };
 
     _.extend(Application.prototype, Backbone.Events, {
+        // events bound on project PubSub
+        projectEvents: {},
+        // events bound on application PubSub
+        events: {},
         initialize: function (project) {
 
         },
+        // called when application is selected.
+        // this is what the setup phase is about.
         setup: function () {
             this.trigger('setup');
         },
+        // called when selected application is changing.
+        // this is used to terminate current application before
+        // the new one is setup.
         teardown: function () {
             this.trigger('teardown');
-        }
+        },
     });
 
-    function initEventListeners (project, application) {
+    function initEventListeners (application) {
+        initProjectEvents(application);
+        initApplicationEvents(application);
+    }
+    function initProjectEvents (application) {
+        var events;
         // listen to project events
-        application.listenTo(project, 'application:change', this.onApplicationChange, this);
+        events = _.extend(
+            application.projectEvents || {},
+            application.options.projectEvents || {}
+        );
+        _.each(events, function (method, eventId) {
+            if (!_.isFunction(method)) {
+                method = application[method];
+            }
+            application.listenTo(application.project, eventId, method, application);
+        });
+    }
+    // listen to application events
+    function initApplicationEvents (application) {
+        var events;
+        events = _.extend(
+            application.events || {},
+            application.options.events || {}
+        );
+        _.each(events, function (method, eventId) {
+            if (!_.isFunction(method)) {
+                method = application[method];
+            }
+            application.listenTo(application, eventId, method, application);
+        });
     }
 
     Application.extend = Backbone.Model.extend;
--- tests/specs/application.js
+++ tests/specs/application.js
@@ -6,40 +6,101 @@ define([
 
     var assert = chai.assert;
 
-    describe('Fossil.Application constructor prototype', function () {
+    describe('Fossil.Application', function () {
+        describe('Fossil.Application constructor prototype', function () {
 
-        it('accepts `path` as second argument', function() {
-            var project = new Project();
-            var app = new Application(project, 'path');
+            it('accepts `path` as second argument', function() {
+                var project = new Project();
+                var app = new Application(project, 'path');
 
-            assert.equal(app.path, 'path');
-            assert.isObject(app.options);
+                assert.equal(app.path, 'path');
+                assert.isObject(app.options);
+            });
+
+            it('accepts `path` as an option', function() {
+                var project = new Project();
+                var app = new Application(project, {path: 'path'});
+
+                assert.equal(app.path, 'path');
+                assert.isObject(app.options);
+            });
+
+            it('sould be possible to give no path nor options', function() {
+                var project = new Project();
+                var app = new Application(project);
+
+                assert.equal(app.path, '');
+                assert.isObject(app.options);
+            });
         });
 
-        it('accepts `path` as an option', function() {
-            var project = new Project();
-            var app = new Application(project, {path: 'path'});
+        describe('Fossil.Application can communicate with project via pubsub', function () {
+            it('proveds access to project pubsub', function(done) {
+                var project = new Project();
+                var app = new Application(project);
 
-            assert.equal(app.path, 'path');
-            assert.isObject(app.options);
+                app.project.on('foo', done);
+                app.project.trigger('foo');
+            });
         });
 
-        it('sould be possible to give no path nor options', function() {
-            var project = new Project();
-            var app = new Application(project);
+        describe('Fossil.Application events registration', function () {
 
-            assert.equal(app.path, '');
-            assert.isObject(app.options);
-        });
-    });
+            it('should regiter events on application pub sub', function (done) {
+                this.timeout(10);
+                done = _.after(2, done);
+                var project = new Project({
+                    applications: {
+                        app1: Application.extend({
+                            events: {
+                                'foo': 'foo',
+                                'bar': function () {
+                                    done();
+                                }
+                            },
+                            projectEvents: {
+                                'foo': function () {
+                                    assert.ok(false, 'It should not register projectEvents in app pubSub');
+                                }
+                            },
+                            foo: function () {
+                                done();
+                            }
+                        })
+                    }
+                });
 
-    describe('Fossil.Application can communicate with project via pubsub', function () {
-        it('proveds access to project pubsub', function(done) {
-            var project = new Project();
-            var app = new Application(project);
+                project.getApplication('app1').trigger('foo');
+                project.getApplication('app1').trigger('bar');
+            });
 
-            app.project.on('foo', done);
-            app.project.trigger('foo');
+            it('should regiter projectEvents on application pub sub', function (done) {
+                this.timeout(10);
+                done = _.after(2, done);
+                var project = new Project({
+                    applications: {
+                        app1: Application.extend({
+                            projectEvents: {
+                                'foo': 'foo',
+                                'bar': function () {
+                                    done();
+                                }
+                            },
+                            events: {
+                                'foo': function () {
+                                    assert.ok(false, 'It should not register events in project pubSub');
+                                }
+                            },
+                            foo: function () {
+                                done();
+                            }
+                        })
+                    }
+                });
+
+                project.trigger('foo');
+                project.trigger('bar');
+            });
         });
     });
 });
