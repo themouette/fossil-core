5faedbf975b07ada70df19ac8404febaafb237c8
Julien Muetton <themouette@gmail.com>
2013-06-17 18:58:53 +0000

    Add a main entry point for Fossil.

---
+++ src/fossil.js
@@ -0,0 +1,10 @@
+define([
+    "fossil/core",
+    "fossil/project",
+    "fossil/application",
+    "fossil/factory",
+    "fossil/factories/routing",
+    "fossil/factories/session"
+], function (Fossil) {
+    return Fossil;
+});
