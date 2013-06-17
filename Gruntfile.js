f1c75e0984bb37f47a083b5e749e6c733bfb27b1
Julien Muetton <themouette@gmail.com>
2013-06-17 02:53:03 +0000

    Add Gruntfile.

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt) {
+
+  console.log(grunt.file.expand({
+    cwd: './modules'
+  }, ['**/compile.js']));
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concurrent: {
+        dev: {
+            tasks: ['watch', 'connect:server:keepalive'],
+            options: {
+                logConcurrentOutput: true
+            }
+        }
+    },
+    watch: {},
+    qunit: {
+        all: {
+            options: {
+                urls: [
+                    'http://localhost:8000/tests/test.html'
+                ]
+            }
+        }
+    },
+    connect: {
+        server: {
+            options: {
+                port: 8000,
+                base: '.',
+                hostname: '*'
+            }
+        }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-concurrent');
+
+  // Default task(s).
+  grunt.registerTask('test', ['connect:server', 'qunit']);
+  grunt.registerTask('dev', ['concurrent:dev']);
+  grunt.registerTask('release', ['test', 'uglify']);
+  grunt.registerTask('default', ['release']);
+
+};
