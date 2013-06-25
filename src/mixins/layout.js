c8c07c4a946b68771d489af7c0903fd3ebd19549
Julien Muetton <themouette@gmail.com>
2013-06-25 13:44:11 +0200

    Introduce Layout mixin.

---
+++ src/mixins/layout.js
@@ -0,0 +1,70 @@
+// the Layout mixin is used to extend a class with layout management.
+// The class should also implment Event mixins.
+//
+// Define your layout in the `template` property of your object.
+// A template can either be a `Backbone.View`, a `function` or a `string`.
+// In any cse it will be transformed into a `Backbone.View` and stored into
+// `layout` property.
+define([
+    'fossil/core',
+    'underscore',
+    'backbone'
+], function (Fossil, _, Backbone) {
+
+    'use strict';
+
+    var LayoutView = Backbone.View.extend({
+        initialize: function (options) {
+            this.template = options.template;
+        },
+        render: function () {
+            this.$el.html(_.result(this.options, 'template'));
+            return this;
+        }
+    });
+
+    var Layout = Fossil.Mixins.Layout = {
+        // use the template property to specify template.
+        template: '',
+        setupLayout: function () {
+            var layout = this.template;
+            if (this.options && this.options.template) {
+                layout = this.options.template;
+            }
+            // place layout property in the object.
+            if (_.isFunction(layout) && !layout.prototype.render) {
+                layout = layout();
+            }
+            if (typeof layout === 'string') {
+                layout = new LayoutView({
+                    template: layout
+                });
+            } else if (layout instanceof Backbone.View) {
+                // nothing to do here
+            } else if (layout.prototype.render) {
+                layout = new layout();
+            }
+            this.layout = layout;
+            this.layout.render();
+            this.trigger('layout:setup');
+        },
+        renderLayout: function ($el) {
+            if (!this.layout) {
+                this.setupLayout();
+            }
+            if ($el) {
+                $el.append(this.layout.$el);
+            }
+            this.trigger('layout:render');
+        },
+        removeLayout: function () {
+            this.layout.$el.detach();
+            this.trigger('layout:remove');
+        },
+        $: function () {
+            return this.layout.$.apply(this.layout, arguments);
+        }
+    };
+
+    return Layout;
+});
---
+++ tests/specs/mixins/layout.js
@@ -0,0 +1,113 @@
+define([
+    'chai',
+    'jquery',
+    'underscore',
+    'fossil/mixins/events',
+    'fossil/mixins/layout'
+], function (chai, $, _, Events, Layoutable) {
+
+    var assert = chai.assert;
+
+    describe('Fossil.Mixins.Layout', function () {
+        // create an object implementing Layoutable
+        var Layout = function (options) {
+            this.options = options || {};
+        };
+        _.extend(Layout.prototype, Events, Layoutable);
+
+        describe('event workflow', function () {
+
+            it('should trigger layout:setup when layout is not initialized', function(done) {
+                this.timeout(10);
+                var l = new Layout();
+                l.on('layout:setup', done);
+                l.renderLayout($('<div />'));
+            });
+
+            it('should trigger layout:setup only once', function() {
+                var l = new Layout();
+                l.renderLayout($('<div />'));
+                l.on('layout:setup', function () {
+                        assert.ok(false, 'layout:setup should have been called already');
+                    });
+                l.removeLayout();
+                l.renderLayout($('<div />'));
+            });
+
+            it('should trigger layout:render every time render is called', function(done) {
+                done = _.after(2, done);
+                this.timeout(10);
+
+                var l = new Layout();
+                l.on('layout:render', done);
+                l.renderLayout($('<div />'));
+                l.removeLayout();
+                l.renderLayout($('<div />'));
+            });
+
+            it('should trigger layout:remove every time remove is called', function(done) {
+                done = _.after(2, done);
+                this.timeout(10);
+
+                var l = new Layout();
+                l.on('layout:remove', done);
+                l.renderLayout($('<div />'));
+                l.removeLayout();
+                l.renderLayout($('<div />'));
+                l.removeLayout();
+            });
+        });
+
+        describe('template property', function () {
+            it('should accept sting template', function () {
+                var l = new Layout({
+                    template: 'foo'
+                });
+                var $el = $('<div/>');
+                l.renderLayout($el);
+                assert.equal($el.html(), '<div>foo</div>');
+            });
+            it('should accept function template', function () {
+                var l = new Layout({
+                    template: function () {
+                        return 'foo';
+                    }
+                });
+                var $el = $('<div/>');
+                l.renderLayout($el);
+                assert.equal($el.html(), '<div>foo</div>');
+            });
+
+            it('should accept view template', function () {
+                var l = new Layout({
+                    template: Backbone.View.extend({
+                        className: 'view',
+                        render: function () {
+                            this.$el.html('foo');
+                            return this;
+                        }
+                    })
+                });
+                var $el = $('<div/>');
+                l.renderLayout($el);
+                assert.equal($el.html(), '<div class="view">foo</div>');
+            });
+
+            it('should accept instanciated view template', function () {
+                var l = new Layout({
+                    template: new (Backbone.View.extend({
+                        className: 'view',
+                        render: function () {
+                            this.$el.html('foo');
+                            return this;
+                        }
+                    }))()
+                });
+                var $el = $('<div/>');
+                l.renderLayout($el);
+                assert.equal($el.html(), '<div class="view">foo</div>');
+            });
+        });
+    });
+
+});
