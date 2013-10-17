46ed2b1a9bb5df4dad5fd3f975807d40d44c79dc
Julien Muetton <themouette@gmail.com>
2013-10-17 23:59:20 +0200

    Add partial session service.

--- tests/specs/services/session.js
+++ tests/specs/services/session.js
@@ -1,57 +1,126 @@
-(function (chai, Application, Module, Session) {
+// TODO: return values !
+define([
+    'assert', 'sinon', 'backbone', 'fossil/module', 'fossil/services/session'
+], function (assert, sinon, Backbone, Module, Session) {
 
-    var assert = chai.assert;
+    suite('Session', function () {
 
-    describe('Fossil.Facroty.Session and Module', function () {
-        it('should be exposed to module', function() {
-            var application = new Application({
-                services: {
-                    'session': Session
-                },
-                modules: {
-                    '': Module
-                }
+        suite('Options', function () {
+            test('#defaults', function() {
+                var defaults = {a:1};
+                var session = new Session({
+                    defaults: defaults
+                });
+
+                assert.equal(session.get('a'), 1);
+            });
+            test('#model', function () {
+                var model = new Backbone.Model({b: 2});
+                var session = new Session({
+                    model: model
+                });
+
+                assert.equal(session.get('b'), 2);
+            });
+        });
+
+        suite('should expose model methods', function() {
+            var model, session, module;
+            setup(function () {
+                model = new Backbone.Model();
+                model.has = sinon.spy();
+                model.get = sinon.spy();
+                model.set = sinon.spy();
+                session = new Session({
+                    model: model
+                });
+                module = new Module();
+            });
+            suite('#get', function() {
+                test('to session', function() {
+                    session.get('foo');
+                });
+                test('as module event', function () {
+                    module.use('session', session);
+
+                    module.trigger('do:get:session', 'foo');
+                });
+                teardown(function () {
+                    assert.ok(model.get.calledOnce);
+                    assert.ok(model.get.calledWith('foo'));
+                });
+            });
+            suite('#has', function() {
+                test('to session', function() {
+                    session.has('foo');
+                });
+                test('as module event', function () {
+                    module.use('session', session);
+
+                    module.trigger('do:has:session', 'foo');
+                });
+                teardown(function () {
+                    assert.ok(model.has.calledOnce);
+                    assert.ok(model.has.calledWith('foo'));
+                });
+            });
+            suite('#set', function() {
+                test('to session', function() {
+                    session.set('foo', 'bar');
+                });
+                test('as module event', function () {
+                    module.use('session', session);
+
+                    module.trigger('do:set:session', 'foo', 'bar');
+                });
+                teardown(function () {
+                    assert.ok(model.set.calledOnce);
+                    assert.ok(model.set.calledWith('foo', 'bar'));
+                });
+            });
+        });
+
+        suite('Module', function () {
+
+            test('should expose model api', function() {
+                var session = new Session();
+                var module = new Module();
+                module.use('session', session);
+
+                assert.isFalse(session.has('foo'));
+
+                session.set('foo', 'bar');
+
+                assert.strictEqual(session.get('foo'), 'bar');
+
+                assert.isTrue(session.has('foo'));
             });
 
-            assert.instanceOf(application.getModule('').services.session, Session);
-        });
-    });
+            test('should expose provided model access', function() {
+                var model = new Backbone.Model();
+                var session = new Session({model: model});
+                var module = new Module();
+                module.use('session', session);
 
-    describe('Fossil.Facroty.Session lifecycle', function () {
+                assert.isFalse(session.has('foo'));
+                assert.isFalse(model.has('foo'));
 
-        it('should throw an error when not initialized with a application', function() {
-            var session = new Session();
+                session.set('foo', 'bar');
 
-            assert.throw(session.get);
-            assert.throw(session.set);
-            assert.throw(session.has);
-        });
+                assert.strictEqual(session.get('foo'), 'bar');
+                assert.strictEqual(model.get('foo'), 'bar');
 
-        it('should throw an error when suspended from application', function() {
-            var session = new Session();
-            var application = new Application();
+                model.set('foo', 'baz');
 
-            session.activateApplication(application);
-            session.suspendApplication(application);
+                assert.strictEqual(session.get('foo'), 'baz');
+                assert.strictEqual(model.get('foo'), 'baz');
 
-            assert.throw(session.get);
-            assert.throw(session.set);
-            assert.throw(session.has);
-        });
-    });
-
-    describe('Fossil.Facroty.Session data storage', function () {
-
-        it('should expose model access', function() {
-            var session = new Session();
-            var application = new Application();
-            session.activateApplication(application);
-
-            assert.isFalse(session.has('foo'));
-            session.set('foo', 'bar');
-            assert.strictEqual(session.get('foo'), 'bar');
+                assert.isTrue(session.has('foo'));
+                assert.isTrue(model.has('foo'));
+            });
         });
 
     });
 
-})(chai, Fossil.Application, Fossil.Module, Fossil.Services.Session);
+
+});
