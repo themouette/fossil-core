0f9c463df925e28802492d4f28718aa07a6760e9
Julien Muetton <themouette@gmail.com>
2013-10-19 22:48:57 +0200

    Add Events service, an application wide event dispatcher.

---
+++ src/services/events.js
@@ -0,0 +1,34 @@
+// services/events is an application wide event dispatcher.
+//
+// It is bound to modules with app! modifier.
+//
+// ``` javascript
+// var app = new Module();
+// var dispatcher = new Events();
+// dispatcher.on('foo', function () {});
+// app.use('events', dispatcher);
+// app.trigger('app!foo');
+// ```
+define(['underscore', '../service'], function (_, Service) {
+
+    var Events = Service.extend({
+        useDeep: true,
+        initialize: function () {
+            _.bindAll(this, 'handle');
+        },
+        use: function (module, parent) {
+            module.addEventModifier('app', this.handle, ['on', 'off', 'once', 'trigger']);
+        },
+        dispose: function (module, parent) {
+            module.removeEventModifier('app');
+        },
+
+        // forward event to
+        handle: function (obj, method, eventname, args) {
+            return this[method].apply(this, [eventname].concat(args));
+        }
+    });
+
+    return Events;
+
+});
