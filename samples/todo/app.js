f8572c55fa06cc42d38e56a50dd167aa6aa79c8c
Julien Muetton <themouette@gmail.com>
2013-11-22 00:20:57 +0100

    todo now uses default selector canvas.

--- samples/todo/app.js
+++ samples/todo/app.js
@@ -2119,7 +2119,8 @@ define('kernel', [
         url = extra.helpers.url(url);
 
         view.once('on:plugins:attach', function () {
-            $('button[data-fossil-id='+id+']').on('click', _.bind(module.navigate, module, url, {trigger: true, replace: true}));
+            $('button[data-fossil-id='+id+']')
+                .on('click', _.bind(module.navigate, module, url, {trigger: true, replace: true}));
 
             view.once('on:plugins:detach', function () {
                 $('button[data-fossil-id='+id+']').off('click');
@@ -2136,8 +2137,6 @@ define('kernel', [
         .use('routing', routing)
         .use('template', template)
         .use('canvas', canvas)
-        .on('start:first', function () {
-            canvas.canvas.defineRegion('content', '.content');
-        })
+        .on('start:first', function () { })
         .start();
 });
