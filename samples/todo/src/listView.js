9294e770aa132326d53e6b190111add605248b25
Julien Muetton <themouette@gmail.com>
2013-11-23 00:52:24 +0100

    Add a way to create a new todo.

--- samples/todo/src/listView.js
+++ samples/todo/src/listView.js
@@ -1,4 +1,7 @@
-define([ 'fossil/views/collection', 'fossil/views/collection' ], function (View, CollectionView) {
+define([
+    'fossil/views/view', 'fossil/views/collection', 'fossil/views/composite'
+], function (View, CollectionView, CompositeView) {
+    "use strict";
 
     var ItemView = View.extend({
         tagName: 'li',
@@ -14,5 +17,37 @@ define([ 'fossil/views/collection', 'fossil/views/collection' ], function (View,
         template: '<ul></lu>'
     });
 
-    return ListView;
+    var FormView = View.extend({
+        tagName: 'p',
+        events: {
+            'submit form': function (e) {
+                e.preventDefault();
+                this.collection.add({
+                    title: this.$('input').val(),
+                    id: this.collection.length
+                });
+                this.$('input[name="title"]').val('');
+            }
+        },
+        initialize: function (options) {
+            this.collection = options.collection;
+        },
+        template: '<form><input type="text" name="title" /><input type="submit" value="Add" /></form>'
+    });
+
+    var MainView = CompositeView.extend({
+        initialize: function (options) {
+            var list = new ListView({
+                collection: options.collection
+            });
+            var form = new FormView({
+                collection: options.collection
+            });
+            this.registerView(form);
+            this.registerView(list);
+        },
+        template: ''
+    });
+
+    return MainView;
 });
