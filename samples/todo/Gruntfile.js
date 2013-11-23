df4c6dbf1b5469d5817c2b32df95df62ddb5e52c
Julien Muetton <themouette@gmail.com>
2013-11-23 18:07:37 +0100

    Use sass and bourbon for an ugly todo theme.

--- samples/todo/Gruntfile.js
+++ samples/todo/Gruntfile.js
@@ -30,14 +30,41 @@ module.exports = function(grunt) {
             kernel: {
                 files: [ 'src/config.js', 'src/kernel.js' ],
                 tasks: ['concat:kernel']
+            },
+            sass: {
+                files: [ 'scss/**/*.{scss,sass}' ],
+                tasks: ['sass:dev']
             }
         },
 
         concurrent: {
             dev: {
-                tasks: ['watch:kernel', 'connect:dev:keepalive'],
+                tasks: ['watch:kernel', 'watch:sass', 'connect:dev:keepalive'],
                 options: {
+                    limit: 3,
                     logConcurrentOutput: true
+                }
+            }
+        },
+
+        sass: {
+            options: {
+                includePaths:['.']
+            },
+            dev: {
+                options: {
+                    outputStyle: 'nested'
+                },
+                files: {
+                    'css/main.css': 'scss/main.scss'
+                }
+            },
+            dist: {
+                options: {
+                    outputStyle: 'compressed'
+                },
+                files: {
+                    'css/main.css': 'scss/main.scss'
                 }
             }
         }
@@ -48,12 +75,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-sass');
 
   // Exposed task(s).
   grunt.registerTask('test', []);
 
-  grunt.registerTask('build:dev', ['concat:kernel']);
-  grunt.registerTask('build:release', ['concat:kernel']);
+  grunt.registerTask('build:dev', ['concat:kernel', 'sass:dev']);
+  grunt.registerTask('build:release', ['concat:kernel', 'sass:dist']);
 
   grunt.registerTask('dev', ['build:dev', 'concurrent:dev']);
   grunt.registerTask('release', ['test', 'build:release']);
