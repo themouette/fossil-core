ee5df802534d4026d60373ee444bd0f36fb32e01
Julien Muetton <themouette@gmail.com>
2013-12-05 23:34:36 +0100

    Move css to sass.

--- samples/mail/Gruntfile.js
+++ samples/mail/Gruntfile.js
@@ -27,18 +27,31 @@ module.exports = function(grunt) {
             }
         },
 
+        sass: {
+            theme: {
+                files: {
+                    'css/main.css': 'scss/main.scss'
+                }
+            }
+        },
+
         watch: {
             kernel: {
                 files: [ 'src/services.js', 'src/config.js', 'src/kernel.js' ],
                 tasks: ['concat:kernel']
+            },
+            theme: {
+                files: [ 'scss/**/*.scss' ],
+                tasks: ['sass:theme']
             }
         },
 
         concurrent: {
             dev: {
-                tasks: ['watch:kernel', 'connect:dev:keepalive'],
+                tasks: ['watch:kernel', 'watch:theme', 'connect:dev:keepalive'],
                 options: {
-                    logConcurrentOutput: true
+                    logConcurrentOutput: true,
+                    limit: 3,
                 }
             }
         }
@@ -49,12 +62,15 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-sass');
 
   // Exposed task(s).
   grunt.registerTask('test', []);
 
-  grunt.registerTask('build:dev', ['concat:kernel']);
-  grunt.registerTask('build:release', ['concat:kernel']);
+  grunt.registerTask('build:theme', 'Build application themes', ['sass']);
+
+  grunt.registerTask('build:dev', ['concat:kernel', 'build:theme']);
+  grunt.registerTask('build:release', ['concat:kernel', 'build:theme']);
 
   grunt.registerTask('dev', ['build:dev', 'concurrent:dev']);
   grunt.registerTask('release', ['test', 'build:release']);
